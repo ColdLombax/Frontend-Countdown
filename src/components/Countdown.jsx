b2865f5a459e739ebf1c8a6efb9178ab5056f3c9
coldlombax <coldlombax@gmail.com>
2021-05-24 23:28:08 +0100

    Template countdown boxs for styling.

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -4,7 +4,30 @@ import './Countdown.scss';
 
 const Countdown = () => (
   <main>
-    <div />
+    <div className="value-container">
+      <div className="box">
+        <p>08</p>
+      </div>
+      <p className="label">DAYS</p>
+    </div>
+    <div className="value-container">
+      <div className="box">
+        <p>23</p>
+      </div>
+      <p className="label">HOURS</p>
+    </div>
+    <div className="value-container">
+      <div className="box">
+        <p>05</p>
+      </div>
+      <p className="label">MINUTES</p>
+    </div>
+    <div className="value-container">
+      <div className="box">
+        <p>45</p>
+      </div>
+      <p className="label">SECONDS</p>
+    </div>
   </main>
 );
 
